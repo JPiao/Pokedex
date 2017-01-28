ddff3bbcaedc13cf913d5c1f6ada3db32d6a8a42
Jason Piao <Jason.piao@hotmail.com>
2017-01-27 23:36:48 -0500

    Update PokeCell.swift

--- Pokedex/PokeCell.swift
+++ Pokedex/PokeCell.swift
@@ -8,24 +8,21 @@
 
 import UIKit
 
-class PokeCell: UICollectionViewCell {
-    
+class PokeCell: UICollectionViewCell {    
     @IBOutlet weak var thumbImg: UIImageView!
     @IBOutlet weak var pokeLbl: UILabel!
     
     var pokemon: Pokemon!
     
+    //Rounding corners
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
         layer.cornerRadius = 5.0
     }
     
     func configureCell(pokemon: Pokemon) {
         self.pokemon = pokemon
-        
         thumbImg.image = UIImage(named: "\(pokemon.pokeId)")
         pokeLbl.text = pokemon.name
-    }
-    
+    } 
 }
